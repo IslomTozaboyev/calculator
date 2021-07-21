f5b38c2150f4f47be56ef729c6a78804d83ae28b
IslomTozaboyev <itozaboyev@gmail.com>
2021-07-21 11:56:32 +0500

    calc

--- src/container/Kalkulator/Kalkulator.js
+++ src/container/Kalkulator/Kalkulator.js
@@ -13,9 +13,21 @@ export default class Kalkulator extends Component {
     constructor(props) {
         super(props);
 
-        this.state= {num1: "", num2: "", answer: ""}
+        this.state= {num1: "", num2: "", answer: "", a: true, b: true}
     }
-    
+    close = () => {
+        this.setState((state) => {
+            return { a: !state.a }
+        })
+    }
+
+    small = () => {
+        this.setState((state) => {
+            console.log(state.b);
+            return { b: !state.b }
+        })
+    }
+
     changed = (event) => {
 
         console.log(event.target.value);
@@ -82,36 +94,38 @@ export default class Kalkulator extends Component {
         })
     }
 
+
     render() {
         return (
-            <div className="container calculator">
+            <div>
+                <div className={`container ${this.state.b && "calculator" || "small__calculator "}`}>
                 <div className="form d-flex justify-content-between align-items-center">
-                    <Button className="reflesh bg-transparent" onClick={this.reflesh}>
+                        <Button className={`${this.state.b && "reflesh bg-transparent text-dark" || "small__reflesh"}`} onClick={this.reflesh}>
                     <FontAwesomeIcon icon={faRedoAlt} />
                     </Button>
-                    <h5 className="fw-bold">Form 1</h5>
-                    <div className="icons">
+                        <h5 className={`${this.state.b && "fw-bold mb-0" || "small__text mb-0"}`}>Form 1</h5>
+                        <div className={`${this.state.b && "icons" || "small__icons"}`}>
                         <Button className="shadow-none me-1"><FontAwesomeIcon icon={faMinus}/></Button>
-                        <Button className="shadow-none "><FontAwesomeIcon icon={faWindowRestore}/></Button>
-                        <Button className="shadow-none me-1"><FontAwesomeIcon icon={faTimes}/></Button>
+                        <Button onClick={this.small} className="shadow-none "><FontAwesomeIcon icon={faWindowRestore}/></Button>
+                        <Button onClick={this.close} className="shadow-none me-1"><FontAwesomeIcon icon={faTimes}/></Button>
                     </div>
                 </div>
 
-               <div className="p-3">
+                {this.state.a && <div className="p-3">
                     <Box className="my-4">
                         <h6>First Number</h6>
-                        <Input className="shadow-none" type="text" onChange={this.changed} value={this.state.num1}/>
+                        <Input className="shadow-none" type="text" onChange={this.changed} value={this.state.num1} />
                     </Box>
                     <Box className="my-4">
                         <h6>Second Number</h6>
-                        <Input className="shadow-none" type="text" onChange={this.changed2} value={this.state.num2}/>
+                        <Input className="shadow-none" type="text" onChange={this.changed2} value={this.state.num2} />
                     </Box>
                     <Box className="my-3">
                         <h6>Answer</h6>
-                            <Input className="shadow-none"  type="text" value={this.state.answer} />
+                        <Input className="shadow-none" type="text" value={this.state.answer} />
                     </Box>
 
-                    <div className="answer text-center">
+                        <div className={`${this.state.b && "answer text-center" || "small__answer text-center"}`}>
                         <Box className="d-flex justify-content-between align-items-center my-4">
                             <Button className="text-dark shadow-none" onClick={this.plus} type="submit" color="secondary">+</Button>
                             <Button className="text-dark shadow-none" onClick={this.minus} color="secondary">-</Button>
@@ -121,14 +135,15 @@ export default class Kalkulator extends Component {
                         <Box className="d-flex justify-content-between align-items-center">
                             <Button className="text-dark shadow-none" onClick={this.bolish} color="secondary">/</Button>
                             <Button className="text-dark shadow-none" onClick={this.kvadrat} color="secondary">
-                                <FontAwesomeIcon icon={faDraft2digital}/>
+                                <FontAwesomeIcon icon={faDraft2digital} />
                             </Button>
                             <Button className="text-dark shadow-none" color="secondary">
-                                <FontAwesomeIcon onClick={this.ildiz} icon={faSquareRootAlt}/>
+                                <FontAwesomeIcon onClick={this.ildiz} icon={faSquareRootAlt} />
                             </Button>
                         </Box>
                     </div>
-                </div>
+                </div> || <div className="text-center mt-5 fw-bold text-danger">Close Window !</div>}
+            </div>
             </div>
         )
     }
